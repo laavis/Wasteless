c0016c6786f52d876e5c1349fbd1508163dabfdc
laavis <sara.suviranta@gmail.com>
2020-01-16 19:25:39 +0200

    create menu icon, start menu styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,49 +14,59 @@ export default () => {
     }
   `);
 
+  const handleMenuOpen = () => {};
+
   return (
     <header className={classes.header}>
       <div>
         <span className={classes.title}>{data.site.siteMetadata.title}</span>
       </div>
-      <ul className={classes.menu}>
-        <li>
-          <Link
-            className={classes.navItem}
-            activeClassName={classes.navItemActive}
-            to="/"
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={classes.navItem}
-            activeClassName={classes.navItemActive}
-            to="/blog"
-          >
-            Blog
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={classes.navItem}
-            activeClassName={classes.navItemActive}
-            to="/about"
-          >
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={classes.navItem}
-            activeClassName={classes.navItemActive}
-            to="/contact"
-          >
-            Contact{' '}
-          </Link>
-        </li>
-      </ul>
+      <div className={classes.menuButton}>
+        <svg className={classes.menuIcon} width="24" height="24">
+          <line x1="2" y1="8" x2="22" y2="8" />
+          <line x1="6" y1="14" x2="22" y2="14" />
+        </svg>
+      </div>
+      <div className={classes.menuOverlay}>
+        <ul className={classes.menu}>
+          <li>
+            <Link
+              className={classes.navItem}
+              activeClassName={classes.navItemActive}
+              to="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={classes.navItem}
+              activeClassName={classes.navItemActive}
+              to="/blog"
+            >
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={classes.navItem}
+              activeClassName={classes.navItemActive}
+              to="/about"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={classes.navItem}
+              activeClassName={classes.navItemActive}
+              to="/contact"
+            >
+              Contact{' '}
+            </Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
